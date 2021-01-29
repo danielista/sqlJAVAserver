c3f84766e82958e4e31a912cb58b496c2a042c61
danielista <61905525+danielista@users.noreply.github.com>
2021-01-29 11:20:14 +0100

    som robil na hodine s učitelom databazy sql ako nato

--- src/sk/kosickaakademia/martinek/databasa.java
+++ src/sk/kosickaakademia/martinek/databasa.java
@@ -8,27 +8,30 @@ import java.sql.ResultSet;
 public class databasa {
 
 
-    public void showCities(String countryCode1, String countryCode2){
+    public void showCities(String KAUNTRI){
 tajnosti tj = new tajnosti();
         try {
-            String query = "SELECT Name, CountryCode " +
+            String query = "SELECT city.Name, city.CountryCode " +
                     "FROM city " +
-                    "WHERE CountryCode LIKE ? OR CountryCode LIKE ?";  //? znamemná že tam dačo chýýýba sú očíslované tie otázniky
+                    "INNER JOIN country ON country.code = city.CountryCode " +
+                    "WHERE country.name LIKE ?";  //? znamemná že tam dačo chýýýba sú očíslované tie otázniky
             //  Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection(tj.getUrl(), tj.getUsername(), tj.getPassword());
             if(conn!=null){
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 System.out.println("SAXES");
                 PreparedStatement ps = conn.prepareStatement(query);
+                System.out.println(ps);
 
-                ps.setString(1,countryCode1);  // ošetrenie.. overuje či to je string..
-                ps.setString(2,countryCode2);  // ošetrenie.. overuje či to je string..
+
+                ps.setString(1,KAUNTRI);  // ošetrenie.. overuje či to je string..
 
 
                 ResultSet rs = ps.executeQuery();  // tu ho už spúšťa
                 while (rs.next()){
                     String city = rs.getString("Name");
                     String code = rs.getString("CountryCode");
+                    String country = rs.getString("CountryCode");
                     System.out.println(city + " " + code);
                 }
                 conn.close();
